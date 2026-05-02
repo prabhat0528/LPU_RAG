a5a2d4e3078e147c22637fc5c07f3b22fd342b62
prabhat0528 <prabhatrai0528@gmail.com>
2026-05-02 17:46:42 +0530

    Added requirements.txt

--- frontend/LPU_RAG/src/App.jsx
+++ frontend/LPU_RAG/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       const res = await axios.post("http://127.0.0.1:5000/ask", {
         query: query,
       });
+      setQuery("")
 
       const botMessage = {
         type: "bot",
